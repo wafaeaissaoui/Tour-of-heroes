11ea9f8a57364688ebc8f4376dc928b36c7099d0
Wafae <wafaeissa17@gmail.com>
2022-03-19 21:01:56 +0100

    first commit

---
+++ src/app/services/logger.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoggerService {
+
+  constructor() { }
+  logs: string[] = []; // capture logs for testing
+
+  log(message: string) {
+    this.logs.push(message);
+    console.log(message);
+  }
+}
